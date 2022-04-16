e81a34c354b5ea6e732ad3ce074b72472ba67c53
Sergio <Sergioamaya538@gmail.com>
2022-04-16 12:32:31 -0700

    async feature

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,8 @@
-from pathlib import Path
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 PATH = "C:\\Users\iamda\\OneDrive\\Desktop\\selenium scripts\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
